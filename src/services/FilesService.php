59c392ea0f5a9ea2da033c81965204f8a4e8a5f0
kiipod <iamkir90@hotmail.com>
2023-02-28 13:16:00 +0300

    Делает страницу редактирования объявления

--- src/services/FilesService.php
+++ src/services/FilesService.php
@@ -2,6 +2,7 @@
 
 namespace buyandsell\services;
 
+use Yii;
 use yii\web\ServerErrorHttpException;
 
 class FilesService
@@ -22,5 +23,19 @@ class FilesService
         }
 
         return $nameFile;
+    }
+
+    /** Метод удаляет фото из папки Uploads
+     *
+     * @param string $file
+     * @return bool
+     */
+    public function deleteFile(string $file): bool
+    {
+        $filePath = Yii::getAlias('@webroot/uploads/image') . $file;
+        if (file_exists($filePath)) {
+            return unlink($filePath);
+        }
+        return false;
     }
 }
